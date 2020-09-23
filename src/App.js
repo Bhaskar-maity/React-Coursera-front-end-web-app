56d2bffe56a58771ec224595fa6eaa11e77bc5df
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-09-23 23:10:46 +0530

    react router, main wrapped by

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import { DISHES} from './shared/dishes';
 
 class App extends Component {
@@ -8,9 +10,11 @@ class App extends Component {
 
   render() {
     return (
-      <div >
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div >
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 
