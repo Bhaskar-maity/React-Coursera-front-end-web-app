f3b92835341d4f7880e982a4cb916391620c945d
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-11-07 23:41:59 +0530

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,7 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import About from "./AboutComponent";
 import DishDetail from './DishDetailComponent';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 // import { COMMENTS } from '../shared/comments';
 // import { PROMOTIONS } from '../shared/promotions';
 // import { LEADERS } from '../shared/leaders';
@@ -25,7 +25,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 
 });
 
@@ -42,27 +43,35 @@ class Main extends Component {
     // }
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
 
+  
   render() {
 
-    const DishWithId = ({match}) => {
-      return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        addComment={this.props.addComment}
-      />
-      );
-    };
-    
     const HomePage = () => {
       return(
           <Home 
-          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
+  
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment}
+          />
+      );
+    };
 
     const AboutPage = () => {
       return <About leaders={this.props.leaders} />;
