0a23fd7ebde83d51eb461613c6cd8e1c735a00d2
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-02-08 21:02:34 +0530

    Updates

---
+++ precache-manifest.ce76f3131bdb64da5d361ae826c2d3b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d295f9b7993dc4756ff7d34293b5877",
+    "url": "/React-Coursera-front-end-web-app/index.html"
+  },
+  {
+    "revision": "8c7cd536082e91fb5e36",
+    "url": "/React-Coursera-front-end-web-app/static/css/2.2f0d7615.chunk.css"
+  },
+  {
+    "revision": "2b1083129f77b020c683",
+    "url": "/React-Coursera-front-end-web-app/static/css/main.05c3950c.chunk.css"
+  },
+  {
+    "revision": "8c7cd536082e91fb5e36",
+    "url": "/React-Coursera-front-end-web-app/static/js/2.2d2abf27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Coursera-front-end-web-app/static/js/2.2d2abf27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1083129f77b020c683",
+    "url": "/React-Coursera-front-end-web-app/static/js/main.07f8c347.chunk.js"
+  },
+  {
+    "revision": "f7642a638173079ce332",
+    "url": "/React-Coursera-front-end-web-app/static/js/runtime-main.da5978f2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Coursera-front-end-web-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Coursera-front-end-web-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Coursera-front-end-web-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Coursera-front-end-web-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Coursera-front-end-web-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
